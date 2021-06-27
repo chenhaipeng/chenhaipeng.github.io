26f58da6d2ef4bc6a884bfa12ead12354d12a4d9
donotcoffee <donotcoffee@gmail.com>
2021-06-27 20:00:10 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,21 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
+#echo 'b.xugaoyi.com' > CNAME
+echo 'www.maincode.icu' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+#  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:chenhaipeng/chenhaipeng.github.io.git
+
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+#  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
+   githubUrl= https://${GITHUB_TOKEN}@github.com/chenhaipeng/chenhaipeng.github.io.git
+
+  git config --global user.name "lost"
+  git config --global user.email "369161356@qq.com"
 fi
 git init
 git add -A
